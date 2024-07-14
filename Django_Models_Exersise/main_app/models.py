d162311fe809cf901a9eb85ad6a1cdd096ab590f
Mario <144950133+Mario8802@users.noreply.github.com>
2024-07-14 23:04:18 +0200

    Update models.py

--- Django_Models_Exersise/main_app/models.py
+++ Django_Models_Exersise/main_app/models.py
@@ -17,5 +17,23 @@ class Blog(models.Model):
 class WeatherForecast(models.Model):
     date = models.DateField()
     temperature = models.FloatField()
+    
     humidity = models.FloatField()
     precipitation = models.FloatField()
+
+
+class Recipe(models.Model):
+    name = models.CharField(
+        max_length=100,
+        unique=True,
+    )
+
+    description = models.TextField()
+
+    ingredients = models.TextField()
+
+    cook_time = models.PositiveIntegerField()
+
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+    )

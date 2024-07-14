fdb92f39b80813b5bb30819df5066be934171742
Mario <144950133+Mario8802@users.noreply.github.com>
2024-07-15 00:35:43 +0200

    Update models.py

--- Django_Models_Exersise/main_app/models.py
+++ Django_Models_Exersise/main_app/models.py
@@ -16,9 +16,11 @@ class Blog(models.Model):
 
 class WeatherForecast(models.Model):
     date = models.DateField()
+
     temperature = models.FloatField()
-    
+
     humidity = models.FloatField()
+
     precipitation = models.FloatField()
 
 
@@ -56,7 +58,6 @@ class Product(models.Model):
     )
 
 
-
 class UserProfile(models.Model):
     username = models.CharField(
         max_length=65,
@@ -75,45 +76,17 @@ class UserProfile(models.Model):
         unique=True,
         default='students@softuni.bg'
     )
-    
+
     bio = models.TextField(
         max_length=120,
     )
-    
+
     profile_image_url = models.URLField(
-        
+
     )
-    
+
     created_at = models.DateTimeField(
         auto_now_add=True,
-    )
-
-
-class Exercise(models.Model):
-    name = models.CharField(
-        max_length=100,
-    )
-
-    description = models.TextField()
-    difficulty_level = models.CharField(
-        max_length=20,
-    )
-
-    equipment = models.CharField(
-        max_length=90,
-    )
-
-    video_url = models.URLField(
-        null=True,
-        blank=True,
-    )
-
-    calories_burned = models.PositiveIntegerField(
-        default=1,
-    )
-
-    is_favourite = models.BooleanField(
-        default=False,
     )
 
 
@@ -147,3 +120,49 @@ class Exercise(models.Model):
         default=False,
     )
 
+
+class GenreChoices(models.TextChoices):
+    FICTION = 'Fiction', 'Fiction'
+    NON_FICTION = 'Non-Fiction', 'Non-Fiction'
+    SCIENCE_FICTION = 'Science Fiction', 'Science Fiction'
+    HORROR = 'Horror', 'Horror'
+
+
+class Book(models.Model):
+    title = models.CharField(
+        max_length=30,
+    )
+
+    author = models.CharField(
+        max_length=100,
+    )
+
+    genre = models.CharField(
+        max_length=20,
+        choices=GenreChoices.choices,
+    )
+
+    publication_date = models.DateField(
+        editable=False,
+        auto_now_add=True
+    )
+
+    price = models.DecimalField(
+        max_digits=8,
+        decimal_places=2,
+    )
+
+    is_available = models.BooleanField(
+        default=True,
+    )
+
+    rating = models.FloatField(
+
+    )
+
+    description = models.TextField(
+
+    )
+
+    def __str__(self):
+        return self.title

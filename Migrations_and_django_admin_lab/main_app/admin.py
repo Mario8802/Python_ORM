3dfe46256a9c192b27c72644825e3ef54108d764
Mario <144950133+Mario8802@users.noreply.github.com>
2024-07-17 10:19:37 +0200

    Add files via upload

---
+++ Migrations_and_django_admin_lab/main_app/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from main_app.models import Product
+
+# admin.site.register(Product)
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+

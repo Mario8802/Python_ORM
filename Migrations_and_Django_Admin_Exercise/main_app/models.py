fcc2b8fed403ab2ed3a37d8612b3fe06938ffa62
Mario <144950133+Mario8802@users.noreply.github.com>
2024-07-19 00:59:09 +0200

    Update models.py

--- Migrations_and_Django_Admin_Exercise/main_app/models.py
+++ Migrations_and_Django_Admin_Exercise/main_app/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.forms import CharField
+
 
 # Create your models here.
 
@@ -21,6 +23,16 @@ class EventRegistration(models.Model):
         return f"{self.participant_name} - {self.event_name}"
 
 
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    director = models.CharField(max_length=100)
+    release_year = models.PositiveIntegerField()
+    genre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"Movie {self.title} by {self.director}"
+
+
 class Student(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
@@ -41,3 +53,16 @@ class Supplier(models.Model):
 
     def __str__(self):
         return f"{self.name} - {self.phone}"
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=90)
+    lecturer = models.CharField(max_length=90)
+    description = models.TextField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    start_date = models.DateField(auto_now_add=True)
+    is_published = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return f"{self.title} - {self.lecturer}"
+    

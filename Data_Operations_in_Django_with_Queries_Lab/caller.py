b8ee78f07dd6cf54d0d9329cce91b1669d784dcf
Mario <144950133+Mario8802@users.noreply.github.com>
2024-07-23 10:35:03 +0200

    Update caller.py

--- Data_Operations_in_Django_with_Queries_Lab/caller.py
+++ Data_Operations_in_Django_with_Queries_Lab/caller.py
@@ -42,3 +42,12 @@ def add_students():
         birth_date='1996-11-25',
         email='bob.wilson@university.com',
     )
+
+
+def get_students_info():
+    result = []
+    all_students = Student.objects.all()
+    for student in all_students:
+        result.append(f"Student №{student.student_id}: {student.first_name} {student.last_name}; Email: {student.email}")
+
+    return '\n'.join(result)

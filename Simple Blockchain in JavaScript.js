44affb74c52b545d17c31d7ef8af0da397078a7e
Mario <144950133+Mario8802@users.noreply.github.com>
2024-09-04 10:42:46 +0200

    Create Simple Blockchain in JavaScript.js

---
+++ Simple Blockchain in JavaScript.js
@@ -0,0 +1,62 @@
+class Block {
+  constructor(index, timestamp, transactions, previousHash = '') {
+    this.index = index;
+    this.timestamp = timestamp;
+    this.transactions = transactions;
+    this.previousHash = previousHash;
+    this.hash = this.calculateHash();
+  }
+
+  calculateHash() {
+    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.transactions)).toString();
+  }
+}
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+    this.pendingTransactions = [];
+    this.miningReward = 100;
+  }
+
+  createGenesisBlock() {
+    return new Block(0, "01/01/2021", "Genesis Block", "0");
+  }
+
+  getLatestBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  minePendingTransactions(miningRewardAddress) {
+    let block = new Block(Date.now(), this.pendingTransactions, this.getLatestBlock().hash);
+    this.chain.push(block);
+
+    this.pendingTransactions = [
+      { fromAddress: null, toAddress: miningRewardAddress, amount: this.miningReward }
+    ];
+  }
+
+  createTransaction(transaction) {
+    this.pendingTransactions.push(transaction);
+  }
+
+  getBalanceOfAddress(address) {
+    let balance = 0;
+
+    for (const block of this.chain) {
+      for (const trans of block.transactions) {
+        if (trans.fromAddress === address) {
+          balance -= trans.amount;
+        }
+
+        if (trans.toAddress === address) {
+          balance += trans.amount;
+        }
+      }
+    }
+
+    return balance;
+  }
+}
+
+const myBlockchain = new Blockchain();
